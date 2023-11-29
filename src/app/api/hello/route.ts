0ce04d7bdc645e063fa774261e024b04511de1ed
vasudevanr <rakesh.vasudevan@agilysys.com>
2023-11-29 18:44:31 +0530

    updates

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,7 +1,7 @@
 //https://logsnag.com/blog/deploying-nextjs-13-app-dir-to-cloudflare-pages
 //https://blog.logrocket.com/using-next-js-route-handlers/
 
-export const dynamic = 'force-dynamic' // defaults to force-static
+export const runtime = 'edge'; // defaults to force-static
 export async function GET(request: Request) {
     return Response.json({ 'rakesh':'welcomes' })
 }
